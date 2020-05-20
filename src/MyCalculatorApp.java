7e94826e90ec345791b0c5095a1bdf371ea3d57f
czzbandicoot <victorsuleiman.1994@gmail.com>
2020-05-19 20:28:16 -0700

    added the final result

--- src/MyCalculatorApp.java
+++ src/MyCalculatorApp.java
@@ -16,6 +16,8 @@ public class MyCalculatorApp {
 	public static void main(String[] args) {
 		int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Hi, I will add 2 numbers for you! Enter the first number:"));
 		int b = Integer.parseInt(JOptionPane.showInputDialog(null, "Now enter the second number:"));
+		
+		JOptionPane.showMessageDialog(null, a + " + " + b + " is equal to " + (a+b));
 
 	}
 
