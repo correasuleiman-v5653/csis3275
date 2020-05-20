687fdda7dec40242b6e791bf57a89e31771a5c86
czzbandicoot <victorsuleiman.1994@gmail.com>
2020-05-19 20:51:02 -0700

    changed addition to subtraction on b1

--- src/MyCalculatorApp.java
+++ src/MyCalculatorApp.java
@@ -14,10 +14,10 @@ public class MyCalculatorApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Hi, I will add 2 numbers for you! Enter the first number:"));
+		int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Hi, I will subtract 2 numbers for you! Enter the first number:"));
 		int b = Integer.parseInt(JOptionPane.showInputDialog(null, "Now enter the second number:"));
 		
-		JOptionPane.showMessageDialog(null, a + " + " + b + " is equal to " + (a+b));
+		JOptionPane.showMessageDialog(null, a + " - " + b + " is equal to " + (a-b));
 
 		//Test successful!
 	}
